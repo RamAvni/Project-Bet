28fd641a79ba7db6f9518e96b6aa02af3256dfe8
RamAvni <ramavni@hilma.tech>
2024-09-30 17:27:51 +0300

    Generate random platforms

--- game.page/index.js
+++ game.page/index.js
@@ -5,37 +5,60 @@ const player = {
   isOnPlatform: false,
 };
 
+const amountOfPlatforms = 8;
 const gameVelocity = 1;
-let platforms = document.getElementsByClassName("platform");
+let nonElementPlatforms = []
 const keysPressedNow = {};
+let platforms = document.getElementsByClassName("platform")
+
+generatePlatforms()
+
 
 function getEntityElement(entity) {
   const entityEl = typeof entity === "object" ? entity.element : entity;
   return entityEl;
 }
 
+function randomInteger(min, max){
+  return Math.floor(Math.random() * (max-min)) + min
+}
+
+function generatePlatforms() {
+  const width = {
+    min: 100,
+    max: 200
+  }
+  const platformHeight = 30;
+  while(platforms.length < amountOfPlatforms){
+    const platformStyle = `
+    width:${randomInteger(width.min, width.max)}px;height:${platformHeight}px;bottom:${randomInteger(0, 100)}%;left:${randomInteger(0, 100)}%;
+    `;
+    document.getElementById("screen-container").innerHTML += (`<div class="platform" style=${platformStyle}></div>`);
+    platforms = document.getElementsByClassName("platform")
+  }
+}
+
+
 function entityMove(entity, direction, amount = 0) {
   // Element OR Object --> X
   // Moves an entity by gameVelocity to the specified direction
   let entityEl = getEntityElement(entity);
-  const rect = entityEl.getBoundingClientRect();
-
   let currentBottomValue = Number(
     getComputedStyle(entityEl).bottom.replace(/px/, "")
   );
-
+  
   let currentLeftValue = Number(
     getComputedStyle(entityEl).left.replace(/px/, "")
   );
-
+  
   switch (direction) {
     case "up":
-    case "ArrowUp":
-    case "Space":
-      entityEl.style.bottom = `${currentBottomValue + gameVelocity + amount}px`;
-      break;
-    case "down":
-    case "ArrowDown":
+      case "ArrowUp":
+        case "Space":
+          entityEl.style.bottom = `${currentBottomValue + gameVelocity + amount}px`;
+          break;
+          case "down":
+            case "ArrowDown":
       entityEl.style.bottom = `${currentBottomValue - gameVelocity - amount}px`;
       break;
     case "left":
@@ -53,15 +76,14 @@ function setKeysPressedNow(e) {
   console.log("press");
   // set
   keysPressedNow[e.code] = true;
-  // Check if jumping
-  if (e.code === "Space" || e.code === "ArrowUp") {
-    jump(player);
-    player.isJumping = true;
-  }
 
   // move player
   for (let property in keysPressedNow) {
-    if (keysPressedNow[property]) {
+    if (e.code === "Space" || e.code === "ArrowUp" || e.code === "up") {
+      jump(player);
+      player.isJumping = true;
+      break;
+    } else if (keysPressedNow[property]) {
       entityMove(player, property, gameVelocity);
     }
   }
@@ -82,10 +104,10 @@ function playerGravity(player) {
   const currentBottomValue = getStrippedNumberFromString(playerStyle.bottom);
   const currentLeftValue = getStrippedNumberFromString(playerStyle.left);
   let onAnyPlatforms = false;
-
+  
   let isOnPlatformTopHeight;
   let isOnPlatformWidth;
-
+  
   for (let platform of platforms) {
     const platformStyle = getComputedStyle(platform);
     const platformBottomValue = getStrippedNumberFromString(
@@ -98,15 +120,15 @@ function playerGravity(player) {
     const platformWidthValue = getStrippedNumberFromString(platformStyle.width);
 
     isOnPlatformTopHeight =
-      currentBottomValue === platformBottomValue + platformHeightValue;
-
+    currentBottomValue === platformBottomValue + platformHeightValue;
+    
     isOnPlatformWidth =
-      platformLeftValue <= currentLeftValue &&
-      currentLeftValue <= platformLeftValue + platformWidthValue;
-
+    platformLeftValue <= currentLeftValue &&
+    currentLeftValue <= platformLeftValue + platformWidthValue;
+    
     if (isOnPlatformTopHeight && isOnPlatformWidth) break;
   }
-
+  
   // Collision
   if (isOnPlatformTopHeight && isOnPlatformWidth) {
     player.isOnPlatform = true;
@@ -122,9 +144,9 @@ function playerGravity(player) {
 }
 
 function jump() {
-  if (!(player.jumpsCount > 5)) {
+  if (player.jumpsCount < 15) {
     player.isJumping = true;
-    entityMove(player, "up", 30);
+    entityMove(player, "up", 10);
   }
 
   player.jumpsCount++;
